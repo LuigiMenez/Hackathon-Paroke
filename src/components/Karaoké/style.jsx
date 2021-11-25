8536715d563aebf9c7ae2ae276253d6b4b85fdf4
Morel Victor <morel.victor1@gmail.com>
2021-11-25 14:20:20 +0100

    Create karaoké compinents and merge app, karaoké

---
+++ src/components/Karaoké/style.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.div`
+  background: red;
+  height: 25vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1``;
+
+export const Video = styled.div`
+  height: 75vh;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Lyrics = styled.div`
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
